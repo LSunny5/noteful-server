cc899fb7babc942b22aed4c7e8346592244011f8
Sunny Lee <leesunny507@gmail.com>
2020-04-27 03:40:18 -0400

    deploy 4

--- src/server.js
+++ src/server.js
@@ -4,10 +4,24 @@ const { PORT, DATABASE_URL } = require('./config')
 
 console.log(DATABASE_URL + 'this is the url connecting to');
 
-const db = knex({
+
+
+const { Client } = require('pg');
+
+const db = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+db.connect();
+
+
+
+
+/* const db = knex({
   client: 'pg',
   connection: DATABASE_URL,
-})
+}) */
 
 app.set('db', db);
 
