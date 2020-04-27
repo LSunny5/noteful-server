59e7a36beec9daf93d7c3603543e632a5fb9c617
Sunny Lee <leesunny507@gmail.com>
2020-04-27 03:16:41 -0400

    deploy 2

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     API_TOKEN: process.env.API_TOKEN || 'false-api-token',
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL_URL || 'postgresql://admin@localhost/noteful',
-    TEST_DATABASE_URL_URL: process.env.TEST_DATABASE_URL_URL || 'postgresql://admin@localhost/noteful-test',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://admin@localhost/noteful',
+    TEST_DATABASE_URL_URL: process.env.TEST_DATABASE_URL || 'postgresql://admin@localhost/noteful-test',
   }
