a84bda11d697a0623d89d838ae88899b4a59c599
Sunny Lee <leesunny507@gmail.com>
2020-04-27 02:57:41 -0400

    deployed to heroku need to fix

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 
+console.log ('connected to : ' + DATABASE_URL)
+
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
   "connectionString": (process.env.NODE_ENV === 'test')
-     ? process.env.TEST_DB_URL
-     : process.env.DB_URL,
+     ? process.env.TEST_DATABASE_URL
+     : process.env.DATABASE_URL_URL,
      "ssl": !!process.env.SSL,
 }
